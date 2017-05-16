22ac126b74397cf43cfa3e1c3f6df2ae0b57f488
Nitish Dolakasharia <nitish.dola@gmail.com>
2017-05-16 13:08:04 +0530

    Test

--- www/js/app.js
+++ www/js/app.js
@@ -36,7 +36,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   
   $scope.selectSource = function() {
-    var input = document.getElementById('tripSource');
+    var input = document.getElementById('tripSource'); alert(input);
     var autocomplete = new google.maps.places.Autocomplete(input);
     google.maps.event.addListener(autocomplete, 'place_changed', function () {
         var place = autocomplete.getPlace();
